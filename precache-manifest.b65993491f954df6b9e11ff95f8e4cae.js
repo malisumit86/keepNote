105e2c82737cb34a5b189576ccce55318342a052
SUMIT <64258056+malisumit86@users.noreply.github.com>
2020-07-11 18:37:50 +0530

    Updates

---
+++ precache-manifest.b65993491f954df6b9e11ff95f8e4cae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f756525b0643fbcaaa9db7352fb7b665",
+    "url": "/keepNote/index.html"
+  },
+  {
+    "revision": "b6bfbd32d965d22dfa4a",
+    "url": "/keepNote/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "341206a1d6ab0cfa5a45",
+    "url": "/keepNote/static/css/main.1e98708b.chunk.css"
+  },
+  {
+    "revision": "b6bfbd32d965d22dfa4a",
+    "url": "/keepNote/static/js/2.b34c8632.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/keepNote/static/js/2.b34c8632.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "341206a1d6ab0cfa5a45",
+    "url": "/keepNote/static/js/main.18589315.chunk.js"
+  },
+  {
+    "revision": "957c9997c34d8d1b4fcd",
+    "url": "/keepNote/static/js/runtime-main.05c01291.js"
+  }
+]);
